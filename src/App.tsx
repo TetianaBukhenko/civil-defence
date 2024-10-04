5b7baafa79374d090731a5dcfbc41681a2fb258d
Tetiana <tania04bukhenko@gmail.com>
2024-10-04 16:18:38 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export const App = () => {
+  return (
+    <h1
+      className=""
+      style={{ color: 'purple' }}
+      onClick={() => {}}
+      onScroll={() => {}}
+    >
+      Basic React App Updated
+    </h1>
+  );
+};
